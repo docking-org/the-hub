ab8ea0f406b725407889328102505135417fc95f
Your Name <you@example.com>
2025-01-12 13:27:22 -0800

    added & in run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 set -m
 
-gunicorn  -b 0.0.0.0:8000 -w 5 --log-level=DEBUG --access-logfile - --error-logfile - application  --timeout 300 --reload
+gunicorn  -b 0.0.0.0:8000 -w 5 --log-level=DEBUG --access-logfile - --error-logfile - application  --timeout 300 --reload &
 
 fg %1
